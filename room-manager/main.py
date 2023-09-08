36266339c0718893eb2435983c2c6e0689501154
ArrayWang <shellmode13@gmail.com>
2023-09-09 02:24:53 +0800

    [lab2-3-matchmaking-service] Init room manager logic

---
+++ room-manager/main.py
@@ -0,0 +1,29 @@
+import json
+
+def main_handler(event, context):
+    try:
+        # Validate request parameters
+        route_key = event.get('requestContext', {}).get('routeKey')
+        connection_id = event.get('requestContext', {}).get('connectionId')
+        event_body = event.get('body')
+        event_body = json.loads(event_body if event_body is not None else '{}')
+
+        if route_key is None or connection_id is None:
+            return {'statusCode': 400}
+
+        if route_key == 'joinroom':
+            print('test joinroom')
+            return {'statusCode': 200}
+
+        if route_key == 'exitroom':
+            print('test exitroom')
+            return {'statusCode': 200}
+
+        if route_key == 'destroyroom':
+            print('test destroyroom')
+            return {'statusCode': 200}
+
+    except Exception as err:
+        print(err)
+        return {'statusCode': 500}
+
